54349ed329aefda5359dd318ecedbfdd30719eb8
Jeff Everhart <Jeffeverhart383@gmail.com>
2018-04-30 08:27:35 -0400

    update to allow full page content to be shown

--- index.php
+++ index.php
@@ -3,7 +3,16 @@
             <div class="map">
 
                 <div id="map">
-
+                <div class="container full-page">
+                    <div class="row">
+                    <div class="col-lg-12">
+                        <?php if(have_posts()): while(have_posts()): the_post(); ?>
+                        <?php $the_content = the_content();
+                        echo $the_content; ?>
+                        <?php endwhile; endif; ?>
+                    </div>
+                    </div>
+                </div>
                 </div>
             </div>
 
