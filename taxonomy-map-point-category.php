d1849db062b95ac716eced27f7b6cfdfd06a5691
Tom Woodward <twwoodward@me.com>
2018-10-02 13:26:19 -0400

    weird issue w this not working on non-logged

--- taxonomy-map-point-category.php
+++ taxonomy-map-point-category.php
@@ -5,7 +5,8 @@ get_header(); ?>
     <h1>This is an</h1>
     <div class="point-list col-md-5">
         <button class="btn btn-primary btn-block" id="toggleHeatmap">Activate Heatmap</button>
-        <input type="hidden" id="category-id" value="<?php get_queried_object()->term_id;?>">
+        <?php $the_cat_id = get_queried_object()->term_id;?>
+        <input type="hidden" id="category-id" value="<?php echo $the_cat_id;?>">
                 <ul>
 
                 <?php if(isset(get_option('map_general_options')['hidden_work']) && !current_user_can('administrator') ): ?>
@@ -15,7 +16,7 @@ get_header(); ?>
                         'post_type' => 'map-point',
                         'author' => get_current_user_id(),
                         'tax_query' => array(
-                            'terms' => get_queried_object()->term_id
+                            'terms' => $the_cat_id
                         )
                     );
                     $query = new WP_Query($args);
@@ -25,7 +26,7 @@ get_header(); ?>
                         <li class="point-tile">
                             <h4><?php the_title(); ?></h4>
                             <p><?php the_excerpt(); ?></p>
-                            <input type="hidden" class="category-id" value="<?php echo get_the_terms($post, 'map-point-category')[0]->term_id; ?>">
+                            <input type="hidden" class="category-id" value="<?php echo $the_cat_id; ?>">
                             <button type="button" class="btn btn-default zoom-button" data-id="<?php echo get_the_ID(); ?>"><i class="fa fa-search"></i></button>
                             <a href="<?php echo get_the_permalink(); ?>" class="btn btn-primary">Read More</a>
                         </li>
@@ -36,6 +37,15 @@ get_header(); ?>
                 <!-- Do Custom Query Here -->
 
                 <?php else: ?>
+                    <?php
+                        $args = array(
+                            'post_type' => 'map-point',
+                            'tax_query' => array(
+                                'terms' => $the_cat_id
+                            )
+                        );
+                        $query = new WP_Query($args);
+                    ?>
                     <?php if (have_posts()): ?>
                         <?php while(have_posts()): the_post(); ?>
                         <li class="point-tile">
